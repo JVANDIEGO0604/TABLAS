6e53510ccac48a0602aba86608e490b94a2eb008
JVANDIEGO0604 <97297843+JVANDIEGO0604@users.noreply.github.com>
2022-01-17 19:10:16 -0500

    Update and rename src/Tabla.java to origen/Tabla.java

--- origen/Tabla.java
+++ origen/Tabla.java
@@ -1,24 +1,36 @@
+import java.util.Scanner;
+
 public class Tabla {
-    int[] salarios = {82, 63, 36, 49, 56, 64, 59, 35, 78, 43, 51, 70, 57, 62, 43, 68, 62, 26, 64, 72, 52, 51, 62, 60, 71, 61, 55, 59, 60, 67, 57, 67, 61, 67, 51, 81, 50, 64, 76, 44, 73, 56, 62, 63, 60};
-
+    
+    
+    int[] salarios = {};
     int Max = 0;
-
     int min = salarios[0];
-
     int R = 0;
-
     int intervalos = 0;
-
     int amplitud = 0;
+    
+    Scanner salario = new Scanner(System.in);
+
+    for(int i=0; i<3; i++)
+        
+    private final Scanner entra = null;
+
+    {
+        System.out.println("escriba los numeros de la tabla");
+        for(int j=0; j<3; j++){
+            int[] salarios = entra.nextInt();
+        }
+    }
 
     public void cantidadSalario(){
-        System.out.println("CANTIDAD DE DATOS (N): ");
+        System.out.println("NUMERO DE DATOS (N): ");
 
         System.out.println(salarios.length);
     }
 
     public void maxSalario(){
-        System.out.println("Maximo valor: ");
+        System.out.println("El maximo valor: ");
 
         for (int i = 0; i < salarios.length; i++) {
             if (salarios[i] > Max)
@@ -28,7 +40,7 @@ public class Tabla {
     }
 
     public void minSalario(){
-        System.out.println("Minimo valor: ");
+        System.out.println("El Minimo valor: ");
 
         for (int i = 0; i < salarios.length; i++) {
             if (salarios[i] < min)
@@ -37,14 +49,14 @@ public class Tabla {
         System.out.println(min);
     }
     public void ranSalario(){
-        System.out.println("Rango de Valores (R): ");
+        System.out.println("Rango: ");
         R= Max - min;
 
         System.out.println(R);
 
     }
     public void interSalario(){
-        System.out.println("Intervalo de Valores(K): ");
+        System.out.println("Intervalo: ");
 
         intervalos = (int) ((1+Math.log(salarios.length))/Math.log(2));
 
@@ -52,7 +64,7 @@ public class Tabla {
 
     }
     public void ampliSalario(){
-        System.out.println("Amplitud (A): ");
+        System.out.println("Amplitud: ");
 
         amplitud = R/intervalos;
 
@@ -69,5 +81,12 @@ public class Tabla {
         salario.interSalario();
         salario.ampliSalario();
 
+    }
+    System.out.println("la transpuesta de esta matriz es: ");
+        for(int i=0; i<100; i++){
+        {
+            System.out.print(int[]salarios + "\t" );
+        }
+        System.out.println("");
     }
 }
